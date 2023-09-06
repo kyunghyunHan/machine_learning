674f7cc8c6e91fca1050a4fed55a708c2e963843
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-09-06 11:12:24 +0900

    feat:read_csv

--- src/ReadSort/read_csv.rs
+++ src/ReadSort/read_csv.rs
@@ -24,13 +24,63 @@ pub struct HousingDataset{
 impl HousingDataset {
     pub fn new(v:Vec<&str> )-> HousingDataset {
         let unwrapped_text:Vec<f64> = v.iter().map(|r|r.parse().unwrap()).collect();
-    
-        }
-}
-pub fn read_hosing_csv(){
+        HousingDataset {
+            zn: unwrapped_text[1], 
+            indus: unwrapped_text[2],
+            chas: unwrapped_text[3],
+            nox: unwrapped_text[4],
+            rm: unwrapped_text[5],
+            age: unwrapped_text[6],
+            dis: unwrapped_text[7],
+            rad: unwrapped_text[8],
+            tax: unwrapped_text[9],
+            ptratio: unwrapped_text[10],
+            black: unwrapped_text[11],
+            lstat: unwrapped_text[12],
+            medv: unwrapped_text[13]} 
+    }
+
+    pub fn train_features(&self)->Vec<f64>{
+        vec![self.crim, 
+        self.zn,
+        self.indus, 
+        self.chas, 
+        self.nox, 
+        self.rm, 
+        self.age, 
+        self.dis, 
+        self.rad, 
+        self.tax, 
+        self.ptratio, 
+        self.black, 
+        self.lstat] // remember we're returning this so no ; 
+    }
+
+    pub fn train_target(&self)->f64 {
+        self.medv
+    }
     
 }
 
+fn read_single_line(s: String) -> HousingDataset { 
+    // read a single line 
+    let raw_vector: Vec<&str> = s.split_whitespace().collect(); 
+    // now read the single vector
+    let housing_vector: HousingDataset = HousingDataset::new(raw_vector); 
+    // now return the vector, no ; 
+    housing_vector
+}
+
+pub fn read_hosing_csv(file_name:impl AsRef<Path>)->Vec<HousingDataset>{
+    let file = File::open(filename).expect("Please, give an input file. Cannot find file");
+    let reader = BufReader::new(file);
+
+    
+    reader.lines().enumerate()
+    .map(|(numb, line)| line.expect(&format!("Impossible to read line number {}", numb)))
+    .map(|row| read_single_line(row))
+    .collect()
+}
 pub fn main(){
 
 }
