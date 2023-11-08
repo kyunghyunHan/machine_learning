59cb9c93fdf40f61b8ef0fe87b68535061114cba
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-11-08 18:08:30 +0900

    feat:to_ndarray

--- src/polars_ch/polars_ch01.rs
+++ src/polars_ch/polars_ch01.rs
@@ -5,13 +5,21 @@ pub fn main(){
     .unwrap()
     .finish()
     .unwrap();
+//SepalLengthCm가 5이상인 것들
 let mask= df.column("SepalLengthCm").unwrap().f64().unwrap().gt(5.0);
+//filter
 let df_small = df.filter(&mask).unwrap();
+//SepalWidthCm와 PetalLengthCm만 출력
 let df_agg = df_small
     .select(["SepalWidthCm","PetalLengthCm"])
     .unwrap();
-println!("{}", df_agg);
+// println!("{}", df_agg);
+//row
+println!("{}", df.height());
+//col
+println!("{}", df.width());
+//ndarray 로 변환
 let ndarray = df_agg.to_ndarray::<Float64Type>(IndexOrder::Fortran).unwrap();
-println!("{}", ndarray);
+// println!("{}", ndarray);
 
 }
