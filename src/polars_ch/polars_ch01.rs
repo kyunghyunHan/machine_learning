a9095235f4a604610012c54c96f66c54803fe962
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-11-08 17:58:08 +0900

    feat: polars

--- src/polars_ch/polars_ch01.rs
+++ src/polars_ch/polars_ch01.rs
@@ -1,19 +1,17 @@
 use polars::prelude::*;
-use ndarray::prelude::*;
-use polars::prelude::DataFrame;
+
 pub fn main(){
     let df = CsvReader::from_path("./datasets/iris.csv")
     .unwrap()
     .finish()
     .unwrap();
-// let mask= df.column("SepalLengthCm").unwrap().f64().unwrap().gt(5.0);
-// let df_small = df.filter(&mask).unwrap();
-// let df_agg = df_small
-//     .group_by(["Species"]).unwrap()
-//     .select(["SepalWidthCm"])
-//     .mean().unwrap();
-println!("{}", df);
-let ndarray = df.to_ndarray::<Float64Type>(IndexOrder::Fortran).unwrap();
+let mask= df.column("SepalLengthCm").unwrap().f64().unwrap().gt(5.0);
+let df_small = df.filter(&mask).unwrap();
+let df_agg = df_small
+    .select(["SepalWidthCm","PetalLengthCm"])
+    .unwrap();
+println!("{}", df_agg);
+let ndarray = df_agg.to_ndarray::<Float64Type>(IndexOrder::Fortran).unwrap();
 println!("{}", ndarray);
 
 }
