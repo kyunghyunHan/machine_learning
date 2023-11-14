e5de708e9ff7c16631df5489ca91ba2310ea813e
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-11-14 11:01:50 +0900

    da

--- src/fish/wine01.rs
+++ src/fish/wine01.rs
@@ -2,10 +2,14 @@ use polars::prelude::*;
 
 pub  fn main(){
 
-    let df = CsvReader::from_path("./datasets/winequalityN.csv")
+    let df = CsvReader::from_path("./datasets/red_wine.csv")
     .unwrap()
     .finish()
     .unwrap();
 
     println!("{}",df);
+    let df_agg = df.select(["alcohol","residual sugar","pH","citric acid"])
+    .unwrap();
+    println!("{}",df_agg);
+ 
 }
