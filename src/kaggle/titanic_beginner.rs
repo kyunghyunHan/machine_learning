e0b0071f56dcdd1febef7e3738001c1f2fa0df43
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-11-15 15:05:50 +0900

    feat:test

--- src/kaggle/titanic_beginner.rs
+++ src/kaggle/titanic_beginner.rs
@@ -9,15 +9,14 @@ use polars::prelude::{CsvWriter, DataFrame, NamedFrom, SerWriter, Series};
 pub fn main(){
     let train_df = CsvReader::from_path("./datasets/titanic_beginner/train.csv")
     .unwrap()
-    .finish()
-    .unwrap().drop_many(&["PassengerId", "Name", "Ticket"]);
+    .finish().unwrap();
    let test_df = CsvReader::from_path("./datasets/titanic_beginner/test.csv")
    .unwrap()
-   .finish()
-   .unwrap().drop_many(&["Name", "Ticket"]);
-
-   println!("{}",train_df.head(None));
-  
+   .finish().unwrap();
+//    .unwrap().drop_many(&["Name", "Ticket"]);
+   //데이터 미리보기
+   println!("{}",train_df.head(Some(5)));
+   train_df.ingo
    let value_counts = train_df.column("Pclass").unwrap().value_counts(false,false).unwrap();
    println!("{}",value_counts);
 
