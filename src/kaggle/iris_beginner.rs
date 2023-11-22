7b828209553b5fcb7f28030f85f1a8eae2f4300f
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-11-22 17:31:49 +0900

    feat:delete species

--- src/kaggle/iris_beginner.rs
+++ src/kaggle/iris_beginner.rs
@@ -26,18 +26,38 @@ fn replace(str_val: &Series) -> Series {
         .into_iter()
         .map(|opt_name: Option<&str>| {
             opt_name.map(|name: &str| if  name =="Iris-setosa" {
-                0 as u32
+                0 as i64
             }else if name =="Iris-virginica"{
                 1
             }else{
                 2
             })
          })
-        .collect::<UInt32Chunked>()
+        .collect::<Int64Chunked>()
         .into_series()
 }
 iris_df.apply("Species", replace).unwrap();
+//데이터 나눈기
+let  data= iris_df.drop("Species").unwrap();//훈련데이터
+let target= iris_df.column("Species").unwrap();//타겟데이터
+println!("{}",data);
 
-    
-println!("{}", &iris_df);
+
+
+let data= data.to_ndarray::<Float64Type>(IndexOrder::Fortran).unwrap();
+
+let mut x_train: Vec<Vec<_>> = Vec::new();
+for row in data.outer_iter() {
+    let row_vec: Vec<_> = row.iter().cloned().collect();
+    x_train.push(row_vec);
+}
+let x_train: DenseMatrix<f64> = DenseMatrix::from_2d_vec(&x_train);
+
+let y_train: Vec<i64> = target.i64().unwrap().into_no_null_iter().collect();
+let y_train:Vec<i32>= y_train.iter().map(|x|*x as i32).collect();
+
+let (train_input, test_input, tarin_target, test_target) = train_test_split(&x_train, &y_train, 0.2,true,None);
+
+
+println!("{}",train_input);
 }
