7290d95ed3a089e7daecbfed33bd6a1fa3ab3d24
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-12-28 10:58:52 +0900

    feat:hose_price_sort

--- src/kaggle/house_price/house_price.rs
+++ src/kaggle/house_price/house_price.rs
@@ -1,4 +1,3 @@
-use std::fs::File;
 use ndarray::prelude::*;
 use polars::prelude::*;
 use polars_core::prelude::*;
@@ -7,7 +6,6 @@ use plotters::prelude::*;
 const OUT_FILE_NAME: &str = "./src/kaggle/house_price/house_price.png";
 
 pub fn main(){
-    println!("{}","hello");
 
     let train_df: DataFrame = CsvReader::from_path("./datasets/house-prices-advanced-regression-techniques/train.csv")
     .unwrap()
@@ -17,15 +15,11 @@ pub fn main(){
     .unwrap();   
     println!("{}",train_df);
 
-    let a=  train_df.column("SalePrice").unwrap();
-    let y_train: Vec<i64> = a.i64().unwrap().into_no_null_iter().collect();
-    let y_train:Vec<f32>= y_train.iter().map(|x|*x as f32).collect();
-    let b: Vec<&Series>= train_df.columns(["YrSold","SalePrice"]).unwrap();
-    println!("{:?}",b);
+
     let mut c= train_df.select(["YrSold","SalePrice"]).unwrap();
-
+    let c= c.sort(&["YrSold"], vec![false, true], true).unwrap();
     let  x_train: ArrayBase<ndarray::OwnedRepr<f32>, Dim<[usize; 2]>> = c.to_ndarray::<Float32Type>(IndexOrder::Fortran).unwrap();
-
+    
     println!("{:?}",x_train);
     let mut data: Vec<Vec<_>> = Vec::new();
     for row in x_train.outer_iter() {
@@ -39,9 +33,16 @@ pub fn main(){
     let root_area: DrawingArea<BitMapBackend<'_>, plotters::coord::Shift> = root_area.titled("House Price", ("sans-serif", 60)).unwrap();
     let mut linear_function = ChartBuilder::on(&root_area)
     .margin(5)
-    .set_all_label_area_size(50)
-    .build_cartesian_2d(147500f32..208500f32, 147500f32..208500f32).unwrap();
+    .set_all_label_area_size(10)
+    .build_cartesian_2d((2005f32..2011f32).step(1f32), (140000.0..208500f32).step(1500f32)).unwrap();
 
+    linear_function
+    .configure_mesh()
+    .disable_mesh()
+    .x_label_formatter(&|v| format!("{:.1}", v))
+    .y_label_formatter(&|v| format!("{:.1}", v))
+    .draw()
+    .unwrap();
 
    
 
@@ -55,4 +56,9 @@ linear_function.draw_series(PointSeries::of_element(
             // + Text::new(format!("{:?}", coord), (0, 15), ("sans-serif", 15))
     },
 )).unwrap();
+// linear_function.draw_series(LineSeries::new(
+//     data.into_iter().map(|x|x).step_by(1).map(|x| (x[0], x[1])), 
+//     &BLUE,
+// ))
+// .unwrap();
 }
