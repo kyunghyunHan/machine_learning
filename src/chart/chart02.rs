04e2bed9240ecd362823d57de6583363614b4f4d
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-01-06 22:39:42 +0900

    feat:test

--- src/chart/chart02.rs
+++ src/chart/chart02.rs
@@ -1,38 +1,47 @@
-/*
-
-제츨파일
-
-PassengerId :PassengerId
-HomePlanet- 승객이 출발한 행성, 일반적으로 영주권이 있는 행성입니다.
-CryoSleep- 승객이 항해 기간 동안 애니메이션을 정지하도록 선택했는지 여부를 나타냅니다. 냉동 수면 중인 승객은 객실에 갇혀 있습니다.
-Cabin- 승객이 머무르는 객실 번호. Port 또는 Starboarddeck/num/side 의 형식 을 취side 합니다 .PS
-Destination- 승객이 내릴 행성.
-Age- 승객의 나이.
-VIP- 승객이 항해 중 특별 VIP 서비스 비용을 지불했는지 여부.
-RoomService, FoodCourt, ShoppingMall, Spa, - 우주선 타이타닉VRDeck 의 다양한 고급 편의 시설 각각에 대해 승객이 청구한 금액입니다 .
-Name- 승객의 이름과 성.
-Transported : target
-*/
-use std::fs::File;
-use ndarray::prelude::*;
-use polars::prelude::*;
-use polars_core::prelude::*;
-use polars_io::prelude::*;
 use plotters::prelude::*;
 
-pub fn main()  {
+const OUT_FILE_NAME: &str = "./src/chart/chart02.png";
+pub fn main() {
+    let root_area = BitMapBackend::new(OUT_FILE_NAME, (600, 400))
+    .into_drawing_area();
+    root_area.fill(&WHITE).unwrap();
 
-    let mut train_df: DataFrame = CsvReader::from_path("./datasets/spaceship_titanic/train.csv")
-    .unwrap()
-    .finish().unwrap();
-
-    println!("{}",train_df);
-
-    let mut test_df: DataFrame = CsvReader::from_path("./datasets/spaceship_titanic/test.csv")
-    .unwrap()
-    .finish().unwrap();
-
-    println!("{}",test_df);
-    // let root: DrawingArea<BitMapBackend<'_>, plotters::coord::Shift> = BitMapBackend::new("./src/chart/chart01.png", (800, 600)).into_drawing_area();
+    let mut ctx = ChartBuilder::on(&root_area)
+        .set_label_area_size(LabelAreaPosition::Left, 40)
+        .set_label_area_size(LabelAreaPosition::Bottom, 40)
+        .caption("Bar Demo", ("sans-serif", 40))
+        .build_cartesian_2d((0..10).into_segmented(), 0..50)
+        .unwrap();
     
+    ctx.configure_mesh().draw().unwrap();
+
+    let data = [25,30];
+    let data1 = [30,40];
+
+    ctx.draw_series((0..2).zip(data.iter()).map(|(x, y)| {
+        let x0 = SegmentValue::Exact(x);
+        let x1 = SegmentValue::Exact(x + 1);
+        let mut bar = Rectangle::new([(x0, 0), (x1, *y)], RED.filled());
+        bar.set_margin(0, 0, 0,0 );
+        bar
+    }))
+    .unwrap();
+ctx.draw_series((0..2).zip(data.iter()).map(|(x, y)| {
+    let x0 = SegmentValue::Exact(x);
+    let x1 = SegmentValue::Exact(x + 1);
+    let mut bar = Rectangle::new([(x0, 0), (x1, *y)], RED.filled());
+    bar.set_margin(0, 0, 0,0 );
+    bar
+}))
+.unwrap();
+
+    ctx.draw_series((2..4).zip(data1.iter()).map(|(x, y)| {
+    let x0 = SegmentValue::Exact(x);
+    let x1 = SegmentValue::Exact(x + 1);
+    let mut bar = Rectangle::new([(x0, 0), (x1, *y)], BLUE.filled());
+    bar.set_margin(0, 0, 0, 0);
+    bar
+    }))
+    .unwrap();
+
 }
--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,15 @@ fn main(){
 
 /*=========chart======== */
 // chart::chart02::main();
-// chart::chart02::main();
+chart::chart02::main();
 // chart::chart03::main();
 // chart::chart04::main();
-chart::linear_function::main();
-chart::step_function::main();
-chart::sigmoid_function::main();
-chart::tanh_function::main();
-chart::relu_function::main();
-chart::sign_function::main();
+// chart::linear_function::main();
+// chart::step_function::main();
+// chart::sigmoid_function::main();
+// chart::tanh_function::main();
+// chart::relu_function::main();
+// chart::sign_function::main();
 /*=========chart======== */
 /*=========fish======== */
 // fish::fish01::main();
