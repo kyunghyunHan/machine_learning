2b450325f9675b8aad1840e2a4ce6961be2ba3f3
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-01-23 21:39:27 +0900

    feat:test

--- src/ds_note/ds_note02.rs
+++ src/ds_note/ds_note02.rs
@@ -6,15 +6,21 @@ use reqwest::Client;
 pub async fn main()-> Result<(), reqwest::Error> {
     let url = "htts://media.githubusercontent.com/media/must-ML10/data_source/main/insurance.csv";
     let client = Client::new();
-
+//   let tf= CsvReader::from_path(path)
     // Make a GET request to the specified URL
     let resp = client.get(url).send().await?;
-
+     
     // Check if the request was successful (status code 2xx)
     if resp.status().is_success() {
         // Read the response body
         let body = resp.text().await?;
-        println!("Response body: {}", body);
+        let reader = CsvReader::new(std::io::Cursor::new(body)).has_header(true);
+
+        // Read the CSV data into a DataFrame
+        let df: DataFrame = reader.finish().unwrap();
+
+        println!("Response body: {}", df);
+        
     } else {
         // Print an error message if the request was not successful
         eprintln!("Request failed with status code: {}", resp.status());
--- src/main.rs
+++ src/main.rs
@@ -41,25 +41,23 @@ mod ds_note;
 
 use polars::prelude::*;
 use reqwest;
-use reqwest::Client;
 #[tokio::main]
 
 pub async fn main()-> Result<(), reqwest::Error> {
     let url = "https://media.githubusercontent.com/media/musthave-ML10/data_source/main/insurance.csv";
-    let client = Client::new();
+    let client = reqwest::Client::new();
 
-    // Make a GET request to the specified URL
-    let resp = client.get(url).send().await?;
+    let resp: reqwest::Response = client.get(url).send().await?;
 
-    // Check if the request was successful (status code 2xx)
-    if resp.status().is_success() {
-        // Read the response body
-        let body = resp.text().await?;
-        println!("Response body: {}", body);
-    } else {
-        // Print an error message if the request was not successful
-        eprintln!("Request failed with status code: {}", resp.status());
-    }
+  
+    let body = resp.bytes().await?;
+
+    let reader = CsvReader::new(std::io::Cursor::new(body)).has_header(true);
+    /*데이터 불러오기 */
+    let df = reader.finish().unwrap();
+
+    println!("{:?}", df);
+   
 
     Ok(())
 
